77bfe7787aea1b328ac34e09b122d100d7b0119b
Penzragon <rifkyaliffa@gmail.com>
2022-03-31 21:44:45 +0700

    Add main controller

---
+++ App/main.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import home
+import prediction
+
+st.set_page_config(
+    page_title="Forest Fire Detection",
+    page_icon="🌳",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        "Get Help": "https://www.linkedin.com/in/rifkyaliffa/",
+        "Report a bug": "https://github.com/Penzragon",
+        "About": "### Simple Plant Diseases Detection App - Kelompok 2",
+    },
+)
+
+PAGES = {"Home": home, "Prediction": prediction}
+
+st.sidebar.title("Navigation")
+page = st.sidebar.selectbox("Choose a page", list(PAGES.keys()))
+
+page = PAGES[page]
+page.app()
